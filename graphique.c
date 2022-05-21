9593c6cb7f1befd81dfe5a2fcac771bb42872047
Laure <laure.grisez@free.fr>
2022-05-21 16:03:44 +0200

    Amélioration de la fenêtre graphique
    Les images ont été réduites pour pouvoir s'afficher sur des écrans de résolution moyenne.

--- graphique.c
+++ graphique.c
@@ -7,33 +7,12 @@
 /* Initialise le moteur graphique SDL2 */
 int initialiser_graphique()
 {
-  if (SDL_InitSubSystem(SDL_INIT_VIDEO) < 0) {
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     SDL_Log("Erreur lors de l'initialisation du graphique: %s\n", SDL_GetError());
     return 1;
   }
-  if (IMG_Init(IMG_INIT_PNG) == 0) {
-    SDL_Log("Erreur lors de l'initialisation de la gestion des images: %s\n", SDL_GetError());
-    return 1;
-  }
-  return 0;
-}
-
-/* Charge le fichier png dans une surface SDL2 */
-int charger_image(char *nom_image, SDL_Surface** surface)
-{
-  *surface = IMG_Load(nom_image);
-  if (*surface == NULL) {
-    SDL_Log("Erreur lors du chargement de l'image %s: %s\n", nom_image, IMG_GetError());
-    return 1;
-  }
-  return 0;
-}
-
-/* Convertit une image en texture */
-int convertir_vers_texture(SDL_Surface *image, SDL_Renderer *renderer, SDL_Texture **texture) {
-  *texture = SDL_CreateTextureFromSurface(renderer, image);
-  if (*texture == NULL) {
-    SDL_Log("Erreur: impossible de créer la texture associée à la surface.\n");
+  if (IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG) {
+    SDL_Log("Error lors de l'initialisation de la gestion des images.\n");
     return 1;
   }
   return 0;
@@ -42,18 +21,18 @@ int convertir_vers_texture(SDL_Surface *image, SDL_Renderer *renderer, SDL_Textu
 /* Charge le fichier png dans une texture SDL2 */
 int charger_texture(char *nom_image, SDL_Renderer * renderer, SDL_Texture **texture)
 {
-  SDL_Surface *surface = IMG_Load(nom_image);
-  if (surface == NULL) {
+  SDL_Surface *image = IMG_Load(nom_image);
+  if (image == NULL) {
     SDL_Log("Erreur lors du chargement de l'image %s: %s\n", nom_image, IMG_GetError());
     return 1;
   }
-  *texture = SDL_CreateTextureFromSurface(renderer, surface);
+  *texture = SDL_CreateTextureFromSurface(renderer, image);
   if (*texture == NULL) {
     SDL_Log("Erreur: impossible de créer la texture associée à la surface.\n");
-    liberer_surface(surface);
+    SDL_FreeSurface(image);
     return 1;
   }
-  liberer_surface(surface);
+  SDL_FreeSurface(image);
   if (SDL_SetTextureBlendMode(*texture, SDL_BLENDMODE_BLEND) != 0) {
     SDL_Log("Erreur: impossible de prendre en compte le cannal alpha de la texture: %s\n", SDL_GetError());
     liberer_texture(*texture);
@@ -74,11 +53,6 @@ int copier_texture(SDL_Texture *texture, SDL_Rect *position, SDL_Renderer *rende
 /* Libère une texture SDL2 */
 void liberer_texture(SDL_Texture *texture) {
   SDL_DestroyTexture(texture);
-}
-
-/* Libère une image SDL2 */
-void liberer_surface(SDL_Surface *image) {
-  SDL_FreeSurface(image);
 }
 
 /* Libère le moteur audio SDL2 */
