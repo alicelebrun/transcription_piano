1de2d6e82d1660b65a36ca451928dd13809c15ca
Alice Lebrun <alice.lebrun2002@gmail.com>
2022-05-21 18:08:23 +0200

    Amélioration du programme principal
    Il enchaîne désormais toutes les étapes depuis la lecture du fichier à analyser
    jusqu'à l'animation graphique de la transcription.

--- transcription_piano.c
+++ transcription_piano.c
@@ -1,55 +1,63 @@
 #include <SDL.h>
-
 #include "son.h"
-#include "interface.h"
 #include "transcription.h"
-#include "note.h"
+#include "interface.h"
 
-int main(int argc, char ** argv) {
-	if(argc != 2) { //Il faut 2 arguments: le nom du programme et le nom de l'audio à traiter
-		printf("même pas en rêve Raymond");
-		return 1;
-		}
-	char* nom_fichier = argv[1]; //nom de l'audio
-	
-	//On initialise le son en vérifiant qu'il n'y a pas eu d'erreur.
-	if(initialiser_son() != 0) {
-		return 1;
-		}
-	
-	SDL_AudioSpec wav_spec;
-	Uint8 *wav_buffer;
-	Uint32 wav_length;
-	
-	//On charge le son en vérifiant qu'il n'y a pas eu d'erreur.
-	if(charger_son(nom_fichier, &wav_spec, &wav_buffer, &wav_length) != 0) {
-		return 1;
-		}
-	
-	double * donnee_son; //amplitude du signal à chaque instant
-	double * instant_son; //instants reliés aux amplitudes
-	int taille;
-	
-	//On convertit le son en vérifiant qu'il n'y a pas eu d'erreur.
-	if(convertir_son(&wav_spec, wav_buffer, wav_length, &donnee_son, &instant_son, &taille) !=0) {
-		return 1;
-		}
-	Uint32 duree = 1000 * instant_son[taille - 1];
-	//On cree le clavier
-	struct clavier_t clavier;
-	
-	creer_clavier(&clavier);
-	
-	//On crée une liste de notes
-	struct liste_note_t * notes;
-	notes = transcrire(donnee_son, instant_son, taille, &clavier);
-	free(donnee_son);
-	free(instant_son);
-	printf("notes=%p\n", notes);
-    SDL_AudioDeviceID deviceID;
-    if (jouer_son(&wav_spec, wav_buffer, wav_length, &deviceID) != 0) {
-      return 1;
-    }
-    SDL_Delay(duree);
-    return 0;
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    printf("Usage: transcription_piano fichier.wav\n");
+    return 1;
+  }
+  char* nom_fichier = argv[1]; //nom de l'audio
+
+  //On initialise le son en vérifiant qu'il n'y a pas eu d'erreur.
+  if (initialiser_son() != 0) {
+    return 1;
+  }
+
+  SDL_AudioSpec wav_spec;
+  Uint8 *wav_buffer;
+  Uint32 wav_length;
+
+  //On charge le son en vérifiant qu'il n'y a pas eu d'erreur.
+  if (charger_son(nom_fichier, &wav_spec, &wav_buffer, &wav_length) != 0) {
+    return 1;
+  }
+
+  double * donnees_son; //amplitude du signal à chaque instant
+  double * instants_son; //instants reliés aux amplitudes
+  int taille;
+
+  //On convertit le son en vérifiant qu'il n'y a pas eu d'erreur.
+  if(convertir_son(&wav_spec, wav_buffer, wav_length, &donnees_son, &instants_son, &taille) !=0) {
+    return 1;
+  }
+  Uint32 duree = 1000 * instants_son[taille - 1];
+  //On cree le clavier
+  struct clavier_t clavier;
+
+  creer_clavier(&clavier);
+
+  // Crée la transcription du signal en liste de notes
+  printf("Transcrire musique\n");
+  struct liste_note_t * notes = transcrire(donnees_son, instants_son, taille, &clavier);
+  // On libère les structures du signal
+  free(donnees_son);
+  free(instants_son);
+  afficher_liste_notes(notes);
+  // Crée l'interface graphique
+  struct interface_t interface;
+  printf("Créer interface\n");
+  if (creer_interface(&interface) != 0) {
+    printf("Erreur: impossible de créer l'interface graphique.\n");
+    return 1;
+  };
+  // Lance l'animation des notes transcrites
+  printf("Animer interface\n");
+  animer_interface(&interface, notes, duree, &wav_spec, wav_buffer, wav_length);
+  // Libère toutes les ressources
+  liberer_son(wav_buffer);
+  liberer_interface(&interface);
+  SDL_Quit();
+  return 0;
 }
